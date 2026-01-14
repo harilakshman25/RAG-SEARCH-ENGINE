1752e78e41600a1894ee78b4dee972d5eae519fa
harilakshman25 <harilakshman509716@kgpian.iitkgp.ac.in>
2026-01-14 11:12:06 +0000

    Query rewrite

--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -29,6 +29,19 @@ def load_stopwords() -> list[str]:
 client = genai.Client(api_key=api_key)
 
 def enhance_query(query: str, method : str) -> str:
+    if method == "spell":
+        enhaned_query = spelling_corrector(query)
+    elif method == "rewrite":
+        enhaned_query = rewriter(query)
+    else:
+        enhaned_query = query
+
+    if query.strip() != enhaned_query:
+        print(f"Enhanced query ({method}): '{query}' -> '{enhaned_query}'\n")
+
+    return enhaned_query
+
+def spelling_corrector(query: str) -> str:
     prompt = f"""Fix any spelling errors in this movie search query.
 
                 Only correct obvious typos. Don't change correctly spelled words.
@@ -42,7 +55,30 @@ def enhance_query(query: str, method : str) -> str:
         model="gemini-2.5-flash", contents=prompt
     )
     enhaned_query = response.text.strip()[len("Corrected:"):].strip().strip('"')
-    if query.strip() != enhaned_query:
-        print(f"Enhanced query ({method}): '{query}' -> '{enhaned_query}'\n")
-
     return enhaned_query
+
+def rewriter(query: str) -> str:
+    prompt = f"""Rewrite this movie search query to be more specific and searchable.
+
+                Original: "{query}"
+
+                Consider:
+                - Common movie knowledge (famous actors, popular films)
+                - Genre conventions (horror = scary, animation = cartoon)
+                - Keep it concise (under 10 words)
+                - It should be a google style search query that's very specific
+                - Don't use boolean logic
+
+                Examples:
+
+                - "that bear movie where leo gets attacked" -> "The Revenant Leonardo DiCaprio bear attack"
+                - "movie about bear in london with marmalade" -> "Paddington London marmalade"
+                - "scary movie with bear from few years ago" -> "bear horror movie 2015-2020"
+
+                Rewritten query:"""
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=prompt
+    )
+    rewritten_query = response.text.strip()[len("Rewritten query:"):].strip().strip('"')
+    return rewritten_query
